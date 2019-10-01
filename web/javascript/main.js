6341cc815089adaeea018b21e6b43ed42cb9f2cd
jhonswai@gmail.com <jhonswai@gmail.com>
2019-10-01 05:53:45 +0300

    drawn a circle with paper.js

--- web/javascript/main.js
+++ web/javascript/main.js
@@ -1,4 +1,11 @@
 // $(document).ready(function(){
     'use strict';
+    paper.install(window);
+    paper.setup(document.getElementById(mainCanvas));
+    // something
+    var c = Shape.Cirle(200,200,50);
+    c.fillColor = "green";
+
+    paper.view.draw();
     console.log("echo");
 // });
