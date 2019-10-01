a6fe5de436b80863c9902d71a3823e68bf156b0c
jhonswai@gmail.com <jhonswai@gmail.com>
2019-10-01 06:17:50 +0300

    added text in canvas

--- web/javascript/main.js
+++ web/javascript/main.js
@@ -7,10 +7,12 @@
     inputTool.onMouseDown = function(event){
         var c = Shape.Cirle(event.point.x,event.point.y,20);
         c.fillColor = "green";
+        let text = new PointText(event.point);
+        text.justification = "center";
+        text.fillColor = "white";
+        text.fontSize = 20;
+        text.content = "Hello user";
     }
-
-   
-
     paper.view.draw();
     console.log("echo");
 // });
