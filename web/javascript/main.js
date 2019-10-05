b7f234d76b3f8bf70ac4161d39c63f44bde07e5b
jhonswai@gmail.com <jhonswai@gmail.com>
2019-10-05 12:15:11 +0300

    changed the paper.js code and commented the rest

--- web/javascript/main.js
+++ web/javascript/main.js
@@ -1,18 +1,30 @@
  $(document).ready(function(){
     'use strict';
-    paper.install(window);
-    paper.setup(document.getElementById(mainCanvas));
+    // paper.install(window);
+    // paper.setup(document.getElementById("mainCanvas"));
     // something
-    let inputTool = new Tool;
-    inputTool.onMouseDown = function(event){
-        var c = Shape.Cirle(event.point.x,event.point.y,20);
-        c.fillColor = "green";
-        let text = new PointText(event.point);
-        text.justification = "center";
-        text.fillColor = "white";
-        text.fontSize = 20;
-        text.content = "Hello user";
-    }
-    paper.view.draw();
-    console.log("echo");
+    // let inputTool = new Tool;
+    // new Shape.Cirle(200,200,50);
+    // inputTool.onMouseDown = function(event){
+    //     var c = Shape.Cirle(event.point.x,event.point.y,20);
+    //     c.fillColor = "green";
+    //     let text = new PointText(event.point);
+    //     text.justification = "center";
+    //     text.fillColor = "white";
+    //     text.fontSize = 20;
+    //     text.content = "Hello user";
+    // }
+    // paper.view.draw();
+    // console.log("echo");
+
+    // creating a paper.js path
+    // let canvas = document.getElementById("mainCanvas");
+    // paper.setup(canvas);
+      let path = new Path();
+      // giving the stroke a fillColor
+      path.strokeColor = 'black';
+      let start = new Point(100,100);
+      path.moveTo(start);
+      path.lineTo(start + [100, -50]);
+
  });
